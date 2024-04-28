608cbf385629b428fe5b76e778af332a322759a6
AndoKami <hei.ando.1007@gmail.com>
2024-04-28 23:23:13 +0300

    add: adding comment and post opperation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import {
-  Admin,
-  Resource,
-  ListGuesser,
-  EditGuesser,
-  ShowGuesser,
-} from "react-admin";
-import { dataProvider } from "./dataProvider";
+import { Admin, EditGuesser, ListGuesser, Resource, ShowGuesser } from 'react-admin';
+import { dataProvider } from './providers/dataProvider';
+import post from './post';
+import comments from './comments';
 
-export const App = () => <Admin dataProvider={dataProvider}></Admin>;
+export const App = () => (
+  <Admin dataProvider={dataProvider}>
+    <Resource name='users' list={ListGuesser} edit={EditGuesser} show={ShowGuesser} />
+    <Resource name='posts' {...post} />
+    <Resource name='comments' {...comments} />
+  </Admin>
+);
